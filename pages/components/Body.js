6af352f3564fa13060eb3e212e887b10e0d6a34e
scorcism <abhishekpathak1720@gmail.com>
2022-07-26 00:02:10 +0530

    Some toggles

--- pages/components/Body.js
+++ pages/components/Body.js
@@ -1,30 +1,42 @@
 const Body = () => {
+
+  const handleClick = () =>{
+
+    let value = document.getElementById('search_bar').value;
+    console.log(value)
+  }
+
+
   return (
     <>
-      <div classNameName="hero">
+      <div className="hero">
         <section className="cntr_pg">
           <img
             className="logo"
             src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
             alt="Google logo"
           />
-          <form action="#" method="#" name="searchquery">
-            <input type="text" name="searchfield" className="search_bar"></input>
+          <div name="searchquery">
+            <div className="select_box">
+            <input type="text" name="searchfield" id="search_bar" className="search_bar"></input>
+            <select className="select" name="cars" id="cars">
+              <option value="Image" className="select_option">Image</option>
+              <option value="PDF" className="select_option">PDF</option>
+              <option value="Song" className="select_option">Song</option>
+              <option value="Book" className="select_option">Book</option>
+            </select>
+            </div>
             <div className="search_buttons">
               <input
                 className="submit_buttons"
                 type="submit"
-                value="Google Search"
+                value="Search"
                 name="submit"
-              />
-              <input
-                className="submit_buttons"
-                type="submit"
-                value="Google Search"
-                name="submit"
+                onClick={handleClick}
+                target='_blank'
               />
             </div>
-          </form>
+          </div>
         </section>
       </div>
     </>
