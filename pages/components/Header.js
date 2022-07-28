19dbd4153909c2ec8742c19588f89b78a8429d29
Rohan Das <rohandasbirbhum@gmail.com>
2022-07-29 01:49:54 +0530

    New UI 💕

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -6,12 +6,15 @@ const Header = () =>{
             
             <div className="navbar-brand">
                 <a href="#">
-                    <h1>GI</h1>
+                <span style={{"color" :"#F4B400",}}>G</span>
+                <span style={{"color" :"#0F9D58",}}>I</span>
                 </a>
             </div>
 
             <div className="navbar-image">
+            <a href="https://github.com/RohanDas28/GIndexer" target={"_blank"}>
             <Image src="/github.png" alt="me" width="64" height="64" />
+            </a>
             </div>
             
         </div>
